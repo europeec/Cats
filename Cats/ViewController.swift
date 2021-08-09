b91600038598a8521fd6e98da1b75c8bc2f0bf88
europeec <l.europeec@ya.ru>
2021-08-09 16:29:21 +0300

    init

---
+++ Cats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cats
+//
+//  Created by Дмитрий Юдин on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
