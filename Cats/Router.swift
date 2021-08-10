e348646a4d34ed761aa3e06dc37b3245057efa8d
europeec <l.europeec@ya.ru>
2021-08-10 19:18:13 +0300

    router

---
+++ Cats/Router.swift
@@ -0,0 +1,49 @@
+//
+//  Router.swift
+//  Cats
+//
+//  Created by Дмитрий Юдин on 10.08.2021.
+//
+
+import UIKit
+
+protocol RouterMainProtocol: AnyObject {
+    var navigationController: UINavigationController? { get set }
+    var builder: BuilderProtocol? { get set }
+    func initialViewController()
+}
+
+protocol RouterProtocol: RouterMainProtocol {
+    func showDetail(cat: Cat)
+    func popToRoot()
+}
+
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+    var builder: BuilderProtocol?
+    
+    init(navigationController: UINavigationController, builder: BuilderProtocol) {
+        self.navigationController = navigationController
+        self.builder = builder
+    }
+    
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let mainViewController = builder?.createMainModule() else { return }
+            navigationController.viewControllers = [mainViewController]
+        }
+    }
+    
+    func showDetail(cat: Cat) {
+        if let navigationController = navigationController {
+            guard let detailViewController = builder?.createDetailModule(with: cat) else { return }
+            navigationController.pushViewController(detailViewController, animated: true)
+        }
+    }
+    
+    func popToRoot() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+}
